72a96d114d5c696ff47e1efd5a736429a97c4124
callmedukey <87065409+callmedukey@users.noreply.github.com>
2024-06-25 09:53:09 +0900

    payment process init

--- app/api/payaction/route.ts
+++ app/api/payaction/route.ts
@@ -1,6 +1,7 @@
 import { verifySession } from "@/actions/session";
 import { formatPhoneNumber } from "@/lib/formatPhoneNumber";
 import type { NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
 //requires phone dashes 010-5555-5555
 export const POST = async (req: NextRequest) => {
   try {
@@ -10,21 +11,93 @@ export const POST = async (req: NextRequest) => {
       return Response.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const { amount, orderId, orderDate, billingName, ordererName, phone } =
+    const { amount, orderDate, billingName, ordererName, phone } =
       await req.json();
+    const student = await prisma.student.findUnique({
+      where: {
+        id: session.userId,
+      },
+      include: {
+        school: true,
+      },
+    });
+    if (!student) {
+      return Response.json({ error: "Student not found" }, { status: 404 });
+    }
+    const shoppingCart = await prisma.savedMeals.findMany({
+      where: {
+        student: {
+          id: session.userId,
+        },
+      },
+    });
+
+    const paymentId = await prisma.$transaction(async (tx) => {
+      const payment = await tx.payments.create({
+        data: {
+          amount,
+          student: {
+            connect: {
+              id: session.userId,
+            },
+          },
+          studentName: student.name,
+          schoolName: student.school.name,
+        },
+      });
+      const createdMeals = await prisma.meals.createMany({
+        data: shoppingCart.map((meal) => ({
+          payment: {
+            connect: {
+              id: payment.id,
+            },
+          },
+          student: {
+            connect: {
+              id: session.userId,
+            },
+          },
+          date: meal.date,
+          mealType: meal.mealType,
+        })),
+      });
+      if (createdMeals && createdMeals.count === shoppingCart.length) {
+        await prisma.savedMeals.deleteMany({
+          where: {
+            student: {
+              id: session.userId,
+            },
+          },
+        });
+
+        return payment.id;
+      } else return null;
+    });
+
+    if (!paymentId) {
+      return Response.json({ error: "Payment failed" }, { status: 400 });
+    }
 
     const payActionUrl = "https://payaction.app/api/1.1/wf/order";
     const body = {
       apikey: process.env.PAYACTION_KEY,
       secretkey: process.env.PAYACTION_SECRET,
       mall_id: process.env.PAYACTION_MALLID,
-      order_number: orderId,
+      order_number: paymentId,
       order_amount: amount,
       order_date: orderDate.toISOString(),
       billing_name: billingName,
       orderer_name: ordererName,
       orderer_phone_number: formatPhoneNumber(phone),
     };
+
+    const response = await fetch(payActionUrl, {
+      method: "POST",
+      body: JSON.stringify(body),
+    });
+
+    console.log(response);
+    return Response.json({ success: true });
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Invalid request" }, { status: 500 });
