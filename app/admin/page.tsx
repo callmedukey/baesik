2765c600a056744ad0e05f0bd29e2dfe63b5494f
callmedukey <87065409+callmedukey@users.noreply.github.com>
2024-06-26 13:27:57 +0900

    basic admin created

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,20 @@
+import { createAdmin } from "@/actions/auth";
 import AdminLoginFormContainer from "@/components/admin/AdminLoginFormContainer";
 import MainContainer from "@/components/layout/main-container";
+import prisma from "@/lib/prisma";
 
-const AdminPage = () => {
+export const dynamic = "force-dynamic";
+
+const AdminPage = async () => {
+  const adminsCount = await prisma.admin.count();
+
+  if (adminsCount === 0) {
+    await createAdmin({
+      username: "admin",
+      password: "admin2024@",
+    });
+  }
+
   return (
     <MainContainer>
       <AdminLoginFormContainer />
